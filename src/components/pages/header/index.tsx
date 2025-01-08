eaab6ea4e85d7cedfbc6c8188a31dfda0a249b39
hasanovtemur05 <hasanovtemur86@gmail.com>
2025-01-08 19:16:38 +0500

    added about

--- src/components/pages/header/index.tsx
+++ src/components/pages/header/index.tsx
@@ -1,13 +1,6 @@
-import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Index = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggleButton = () => {
-    setOpen(!open);
-  };
 
   return (
     <>
@@ -19,66 +12,6 @@ const Index = () => {
           >
             ANDMI
           </Link>
-
-          <div className="md:hidden">
-            <button
-              onClick={toggleButton}
-              className="text-gray-600 text-[18px] focus:outline-none"
-            >
-              {open ? <CloseOutlined /> : <MenuOutlined />}
-            </button>
-          </div>
-
-          <nav
-            className={`fixed top-0 left-0 w-[50%] sm:w-[40%] h-full bg-gray-200 shadow-lg transform ${
-              open ? "translate-x-0" : "-translate-x-full"
-            } transition-transform duration-300 ease-in-out md:static md:transform-none md:flex md:items-center md:gap-8 md:w-auto md:h-auto md:shadow-none`}
-          >
-            <Link
-              to="/user-layout"
-              className="font-medium text-gray-600 text-[18px] md:hidden block pt-4 px-8"
-            >
-              ANDMI
-            </Link>
-            <ul className="flex flex-col md:flex-row md:items-center md:gap-8 p-5 md:p-0">
-              <li>
-                <Link
-                  to="/user-layout"
-                  className="block py-3 px-4 text-gray-600 hover:text-gray-800 hover:bg-gray-300 rounded md:hover:bg-transparent"
-                  onClick={() => setOpen(false)}
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="block py-3 px-4 text-gray-600 hover:text-gray-800 hover:bg-gray-300 rounded md:hover:bg-transparent"
-                  onClick={() => setOpen(false)}
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="block py-3 px-4 text-gray-600 hover:text-gray-800 hover:bg-gray-300 rounded md:hover:bg-transparent"
-                  onClick={() => setOpen(false)}
-                >
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="block py-3 px-4 text-gray-600 hover:text-gray-800 hover:bg-gray-300 rounded md:hover:bg-transparent"
-                  onClick={() => setOpen(false)}
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
         </div>
       </header>
     </>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,13 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from "../App";
-import { AdminLayout, Products, Scan, Settings, SignIn, SignUp, TaskLists, TasksCompleted, UserLayout } from "../modules";
+import { AdminLayout, Products, Scan, Settings, SignIn, TaskLists, TasksCompleted, UserLayout } from "../modules";
 const Index = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
     <Route path="/" element={<App />}>
         <Route index element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
         <Route path="/user-layout" element={<UserLayout/>}>
           <Route path="products" element={<Products/>}/>
           <Route path="task-lists" element={<TaskLists/>}/>
