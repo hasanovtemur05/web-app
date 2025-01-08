a4a48f82432675925bca5fcfcb4de76d7263d3f2
hasanovtemur05 <hasanovtemur86@gmail.com>
2025-01-08 18:17:17 +0500

    added about

--- src/modules/scaner/pages/index.tsx
+++ src/modules/scaner/pages/index.tsx
@@ -6,6 +6,10 @@ const Index = () => {
 
   useEffect(() => {
     const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
       fps: 5,
     }, false);
 
